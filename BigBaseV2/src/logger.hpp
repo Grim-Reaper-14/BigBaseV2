017f7c1536c44ae88922ccd0e4d123cd2c569e82
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-04 18:49:06 -0400

    core: harden logger and add warning level

--- BigBaseV2/src/logger.hpp
+++ BigBaseV2/src/logger.hpp
@@ -1,5 +1,21 @@
 #pragma once
-#include "common.hpp"
+
+#include <Windows.h>
+
+#include <chrono>
+#include <cstdint>
+#include <cstdlib>
+#include <filesystem>
+#include <fstream>
+#include <iomanip>
+#include <mutex>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <type_traits>
+#include <utility>
+
+#include <fmt/format.h>
 
 namespace big
 {
@@ -8,181 +24,209 @@ namespace big
 		red = FOREGROUND_RED,
 		green = FOREGROUND_GREEN,
 		blue = FOREGROUND_BLUE,
+		yellow = FOREGROUND_RED | FOREGROUND_GREEN,
+		white = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE,
 		intensify = FOREGROUND_INTENSITY
 	};
 
-	inline log_color operator|(log_color a, log_color b)
+	[[nodiscard]] inline constexpr log_color operator|(log_color left, log_color right) noexcept
 	{
-		return static_cast<log_color>(static_cast<std::underlying_type_t<log_color>>(a) | static_cast<std::underlying_type_t<log_color>>(b));
+		using underlying = std::underlying_type_t<log_color>;
+		return static_cast<log_color>(static_cast<underlying>(left) | static_cast<underlying>(right));
 	}
 
 	class logger;
-	inline logger *g_logger{};
+	inline logger* g_logger{};
 
-	class logger
+	class logger final
 	{
 	public:
-		explicit logger() :
-			m_file_path(std::getenv("appdata"))
+		logger()
 		{
-			m_file_path /= "BigBaseV2";
-			try
-			{
-				if (!std::filesystem::exists(m_file_path))
-				{
-					std::filesystem::create_directory(m_file_path);
-				}
-				else if (!std::filesystem::is_directory(m_file_path))
-				{
-					std::filesystem::remove(m_file_path);
-					std::filesystem::create_directory(m_file_path);
-				}
-
-				m_file_path /= "BigBaseV2.log";
-				m_file_out.open(m_file_path, std::ios_base::out | std::ios_base::app);
-			}
-			catch (std::filesystem::filesystem_error const&)
-			{
-			}
-
-			if ((m_did_console_exist = AttachConsole(GetCurrentProcessId())) == false)
-				AllocConsole();
-
-			if ((m_console_handle = GetStdHandle(STD_OUTPUT_HANDLE)) != nullptr)
-			{
-				SetConsoleTitleA("BigBaseV2");
-				SetConsoleOutputCP(CP_UTF8);
-			
-				m_console_out.open("CONOUT$", std::ios_base::out | std::ios_base::app);
-			}
-
+			initialize_file();
+			initialize_console();
 			g_logger = this;
 		}
 
 		~logger()
 		{
-			if (!m_did_console_exist)
-				FreeConsole();
+			std::scoped_lock lock(m_mutex);
+			if (g_logger == this)
+				g_logger = nullptr;
 
-			g_logger = nullptr;
+			m_console_out.close();
+			m_file_out.close();
+
+			if (!m_console_preexisted && m_console_allocated)
+				FreeConsole();
 		}
 
-		template <typename ...Args>
-		void raw(log_color color, Args const &...args)
+		logger(const logger&) = delete;
+		logger(logger&&) = delete;
+		logger& operator=(const logger&) = delete;
+		logger& operator=(logger&&) = delete;
+
+		template <typename... Args>
+		void raw(log_color color, const Args&... args)
 		{
+			std::scoped_lock lock(m_mutex);
 			raw_to_console(color, args...);
 			raw_to_file(args...);
 		}
 
-		template <typename ...Args>
-		void log(log_color color, std::string_view prefix, std::string_view format, Args const &...args)
+		template <typename... Args>
+		void log(log_color color, std::string_view prefix, fmt::format_string<Args...> format, Args&&... args)
 		{
-			auto message = fmt::format(format, args...);
-		
-			auto time_since_epoch = std::time(nullptr);
-			auto local_time = std::localtime(&time_since_epoch);
-			
-			auto console_timestamp = fmt::format("[{:0>2}:{:0>2}:{:0>2}]", local_time->tm_hour, local_time->tm_min, local_time->tm_sec);
-			auto file_timestamp = fmt::format("[{}-{}-{} {:0>2}:{:0>2}:{:0>2}]", local_time->tm_year + 1900, local_time->tm_mon + 1, local_time->tm_mday, local_time->tm_hour, local_time->tm_min, local_time->tm_sec);
+			const auto message = fmt::format(format, std::forward<Args>(args)...);
+			const auto timestamps = make_timestamps();
 
-			raw_to_console(color, console_timestamp, " [", prefix, "] ", message, "\n");
-			raw_to_file(file_timestamp, " [", prefix, "] ", message, "\n");
+			std::scoped_lock lock(m_mutex);
+			raw_to_console(color, timestamps.console, " [", prefix, "] ", message, "\n");
+			raw_to_file(timestamps.file, " [", prefix, "] ", message, "\n");
 		}
+
+		[[nodiscard]] const std::filesystem::path& file_path() const noexcept
+		{
+			return m_file_path;
+		}
+
 	private:
-		template <typename ...Args>
-		void raw_to_console(log_color color, Args const &...args)
+		struct timestamp_pair final
+		{
+			std::string console;
+			std::string file;
+		};
+
+		static timestamp_pair make_timestamps()
+		{
+			const auto now = std::chrono::system_clock::now();
+			const auto time = std::chrono::system_clock::to_time_t(now);
+			std::tm local{};
+			localtime_s(&local, &time);
+
+			return {
+				fmt::format("[{:02}:{:02}:{:02}]", local.tm_hour, local.tm_min, local.tm_sec),
+				fmt::format("[{:04}-{:02}-{:02} {:02}:{:02}:{:02}]",
+					local.tm_year + 1900,
+					local.tm_mon + 1,
+					local.tm_mday,
+					local.tm_hour,
+					local.tm_min,
+					local.tm_sec)
+			};
+		}
+
+		void initialize_file() noexcept
+		{
+			try
+			{
+				const char* appdata = std::getenv("APPDATA");
+				m_file_path = appdata && *appdata
+					? std::filesystem::path(appdata) / "BigBaseV2"
+					: std::filesystem::temp_directory_path() / "BigBaseV2";
+
+				std::error_code error;
+				std::filesystem::create_directories(m_file_path, error);
+				if (error)
+					return;
+
+				m_file_path /= "BigBaseV2.log";
+				m_file_out.open(m_file_path, std::ios::out | std::ios::app);
+			}
+			catch (...)
+			{
+				m_file_path.clear();
+			}
+		}
+
+		void initialize_console() noexcept
+		{
+			m_console_preexisted = AttachConsole(ATTACH_PARENT_PROCESS) != FALSE;
+			if (!m_console_preexisted)
+				m_console_allocated = AllocConsole() != FALSE;
+
+			m_console_handle = GetStdHandle(STD_OUTPUT_HANDLE);
+			if (!m_console_handle || m_console_handle == INVALID_HANDLE_VALUE)
+			{
+				m_console_handle = nullptr;
+				return;
+			}
+
+			SetConsoleTitleA("BigBaseV2 Enhanced");
+			SetConsoleOutputCP(CP_UTF8);
+			m_console_out.open("CONOUT$", std::ios::out | std::ios::app);
+		}
+
+		template <typename... Args>
+		void raw_to_console(log_color color, const Args&... args)
 		{
 			if (m_console_handle)
-			{
-				SetConsoleTextAttribute(m_console_handle, static_cast<std::uint16_t>(color));
-			}
+				SetConsoleTextAttribute(m_console_handle, static_cast<WORD>(color));
 
 			if (m_console_out)
 			{
 				((m_console_out << args), ...);
-				m_console_out << std::flush;
+				m_console_out.flush();
 			}
 		}
 
-		template <typename ...Args>
-		void raw_to_file(Args const &...args)
+		template <typename... Args>
+		void raw_to_file(const Args&... args)
 		{
 			if (m_file_out)
 			{
 				((m_file_out << args), ...);
-				m_file_out << std::flush;
+				m_file_out.flush();
 			}
 		}
-	private:
-		bool m_did_console_exist{};
+
+		mutable std::mutex m_mutex;
+		bool m_console_preexisted{};
+		bool m_console_allocated{};
 		HANDLE m_console_handle{};
 		std::ofstream m_console_out;
-
 		std::filesystem::path m_file_path;
 		std::ofstream m_file_out;
 	};
 
-	template <typename ...Args>
-	inline void log_info(std::string_view format, Args const &...args)
+	template <typename... Args>
+	inline void log_info(fmt::format_string<Args...> format, Args&&... args)
 	{
 		if (g_logger)
-		{
-			g_logger->log(log_color::blue | log_color::intensify, "Info", format, args...);
-		}
-		else
-		{
-			DebugBreak();
-		}
+			g_logger->log(log_color::blue | log_color::green | log_color::intensify, "Info", format, std::forward<Args>(args)...);
 	}
 
-	template <typename ...Args>
-	inline void log_error(std::string_view format, Args const &...args)
+	template <typename... Args>
+	inline void log_warning(fmt::format_string<Args...> format, Args&&... args)
 	{
 		if (g_logger)
-		{
-			g_logger->log(log_color::green | log_color::intensify, "Error", format, args...);
-		}
-		else
-		{
-			DebugBreak();
-		}
+			g_logger->log(log_color::yellow | log_color::intensify, "Warning", format, std::forward<Args>(args)...);
 	}
 
-	template <typename ...Args>
-	inline void log_trace(std::string_view format, Args const &...args)
+	template <typename... Args>
+	inline void log_error(fmt::format_string<Args...> format, Args&&... args)
 	{
 		if (g_logger)
-		{
-			g_logger->log(log_color::green | log_color::intensify, "Trace", format, args...);
-		}
-		else
-		{
-			DebugBreak();
-		}
+			g_logger->log(log_color::red | log_color::intensify, "Error", format, std::forward<Args>(args)...);
 	}
 
-	template <typename ...Args>
-	inline void log_raw(log_color color, Args const &...args)
+	template <typename... Args>
+	inline void log_trace(fmt::format_string<Args...> format, Args&&... args)
 	{
 		if (g_logger)
-		{
+			g_logger->log(log_color::white, "Trace", format, std::forward<Args>(args)...);
+	}
+
+	template <typename... Args>
+	inline void log_raw(log_color color, const Args&... args)
+	{
+		if (g_logger)
 			g_logger->raw(color, args...);
-		}
-		else
-		{
-			DebugBreak();
-		}
 	}
-
-#define LOG_INFO_IMPL(format, ...) (::big::log_info(format, __VA_ARGS__))
-#define LOG_INFO(format, ...) LOG_INFO_IMPL(format, __VA_ARGS__)
-
-#define LOG_ERROR_IMPL(format, ...) (::big::log_error(format, __VA_ARGS__))
-#define LOG_ERROR(format, ...) LOG_ERROR_IMPL(format, __VA_ARGS__)
-
-#define LOG_TRACE_IMPL(format, ...) (::big::log_trace(format, __VA_ARGS__))
-#define LOG_TRACE(format, ...) LOG_TRACE_IMPL(format, __VA_ARGS__)
-
-#define LOG_RAW_IMPL(color, ...) (::big::log_raw(color, __VA_ARGS__))
-#define LOG_RAW(color, ...) LOG_RAW_IMPL(color, __VA_ARGS__)
 }
+
+#define LOG_INFO(...) (::big::log_info(__VA_ARGS__))
+#define LOG_WARNING(...) (::big::log_warning(__VA_ARGS__))
+#define LOG_ERROR(...) (::big::log_error(__VA_ARGS__))
+#define LOG_TRACE(...) (::big::log_trace(__VA_ARGS__))
+#define LOG_RAW(...) (::big::log_raw(__VA_ARGS__))
