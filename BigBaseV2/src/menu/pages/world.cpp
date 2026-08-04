b95a897a3421d6fe1ef83e3a832960475e21ee05
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-04 12:17:56 -0400

    menu: redesign world page

--- BigBaseV2/src/menu/pages/world.cpp
+++ BigBaseV2/src/menu/pages/world.cpp
@@ -1,4 +1,5 @@
 #include "world.hpp"
+#include "../widgets.hpp"
 
 #include <imgui.h>
 
@@ -6,24 +7,32 @@ namespace big::menu_pages
 {
 	void draw_world()
 	{
-		ImGui::Text("World");
-		ImGui::Separator();
+		menu_ui::page_title("World", "Control time, weather, gravity, traffic, and ambient population settings.");
 
-		ImGui::TextDisabled("Time");
-		ImGui::Checkbox("Freeze Time", &g_world_settings.freeze_time);
-		ImGui::SliderInt("Hour", &g_world_settings.hour, 0, 23);
-		ImGui::SliderInt("Minute", &g_world_settings.minute, 0, 59);
-		ImGui::SliderFloat("Time Scale", &g_world_settings.time_scale, 0.1f, 2.0f, "%.1fx");
-
-		ImGui::Spacing();
-		ImGui::TextDisabled("Environment");
-		ImGui::Checkbox("Clear Weather", &g_world_settings.clear_weather);
-		ImGui::Checkbox("Blackout", &g_world_settings.blackout);
-		ImGui::Checkbox("Low Gravity", &g_world_settings.low_gravity);
+		if (menu_ui::begin_section("WorldTime", "Time", 145.0f))
+		{
+			ImGui::Checkbox("Freeze Time", &g_world_settings.freeze_time);
+			ImGui::SliderInt("Hour", &g_world_settings.hour, 0, 23);
+			ImGui::SliderInt("Minute", &g_world_settings.minute, 0, 59);
+			ImGui::SliderFloat("Time Scale", &g_world_settings.time_scale, 0.1f, 2.0f, "%.1fx");
+		}
+		menu_ui::end_section();
 
 		ImGui::Spacing();
-		ImGui::TextDisabled("Population");
-		ImGui::Checkbox("Disable Traffic", &g_world_settings.disable_traffic);
-		ImGui::Checkbox("Disable Pedestrians", &g_world_settings.disable_pedestrians);
+		if (menu_ui::begin_section("WorldEnvironment", "Environment", 130.0f))
+		{
+			ImGui::Checkbox("Clear Weather", &g_world_settings.clear_weather);
+			ImGui::Checkbox("Blackout", &g_world_settings.blackout);
+			ImGui::Checkbox("Low Gravity", &g_world_settings.low_gravity);
+		}
+		menu_ui::end_section();
+
+		ImGui::Spacing();
+		if (menu_ui::begin_section("WorldPopulation", "Population", 105.0f))
+		{
+			ImGui::Checkbox("Disable Traffic", &g_world_settings.disable_traffic);
+			ImGui::Checkbox("Disable Pedestrians", &g_world_settings.disable_pedestrians);
+		}
+		menu_ui::end_section();
 	}
 }
