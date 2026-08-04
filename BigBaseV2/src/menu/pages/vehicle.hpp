2239208eb98fb4d0f9057143fb7daa26dd516139
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-04 11:44:14 -0400

    vehicle: add native spawner state

--- BigBaseV2/src/menu/pages/vehicle.hpp
+++ BigBaseV2/src/menu/pages/vehicle.hpp
@@ -1,7 +1,20 @@
 #pragma once
 
+#include <array>
+#include <atomic>
+#include <string>
+
 namespace big::menu_pages
 {
+	enum class vehicle_spawn_status
+	{
+		idle,
+		loading,
+		spawned,
+		invalid_model,
+		load_failed
+	};
+
 	struct vehicle_settings final
 	{
 		bool god_mode{};
@@ -11,9 +24,15 @@ namespace big::menu_pages
 		bool rainbow_paint{};
 		float acceleration_multiplier{1.0f};
 		float gravity_multiplier{1.0f};
+
+		std::array<char, 64> spawn_model{"adder"};
+		bool spawn_inside{true};
+		bool spawn_networked{true};
+		std::atomic<vehicle_spawn_status> spawn_status{vehicle_spawn_status::idle};
 	};
 
 	inline vehicle_settings g_vehicle_settings;
 
 	void draw_vehicle();
+	void queue_vehicle_spawn(std::string model_name, bool put_player_inside, bool networked);
 }
