6bd8e332a5f882f51084c69f2aa2041cd8094a76
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-04 11:48:30 -0400

    natives: add cache validation and statistics

--- BigBaseV2/src/invoker.hpp
+++ BigBaseV2/src/invoker.hpp
@@ -13,16 +13,35 @@ namespace big
 		std::uint64_t m_arg_stack[100];
 	};
 
+	struct native_cache_stats final
+	{
+		std::size_t mappings{};
+		std::size_t cached{};
+		std::size_t missing{};
+		std::size_t duplicate_original_hashes{};
+		std::size_t direct_hash_fallbacks{};
+
+		[[nodiscard]] bool healthy() const noexcept
+		{
+			return mappings > 0 && cached > 0 && missing == 0 && duplicate_original_hashes == 0;
+		}
+	};
+
 	class native_invoker
 	{
 	public:
 		explicit native_invoker() = default;
 		~native_invoker() = default;
 
-		void cache_handlers();
+		native_cache_stats cache_handlers();
 
 		void begin_call();
 		void end_call(rage::scrNativeHash hash);
+
+		[[nodiscard]] const native_cache_stats& cache_stats() const noexcept
+		{
+			return m_cache_stats;
+		}
 
 		template <typename T>
 		void push_arg(T &&value)
@@ -38,6 +57,7 @@ namespace big
 	private:
 		native_call_context m_call_context;
 		std::unordered_map<rage::scrNativeHash, rage::scrNativeHandler> m_handler_cache;
+		native_cache_stats m_cache_stats{};
 	};
 
 	inline native_invoker g_native_invoker;
