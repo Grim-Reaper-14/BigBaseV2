1fc7d4feb29c11f1ef86a751d77afacdb12abce4
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-04 18:14:01 -0400

    runtime: harden fiber job pool ownership

--- BigBaseV2/src/fiber_pool.hpp
+++ BigBaseV2/src/fiber_pool.hpp
@@ -3,20 +3,33 @@
 
 namespace big
 {
-	class fiber_pool
+	class fiber_pool final
 	{
 	public:
 		explicit fiber_pool(std::size_t num_fibers);
 		~fiber_pool();
 
-		void queue_job(std::function<void()> func);
+		fiber_pool(const fiber_pool&) = delete;
+		fiber_pool(fiber_pool&&) = delete;
+		fiber_pool& operator=(const fiber_pool&) = delete;
+		fiber_pool& operator=(fiber_pool&&) = delete;
 
+		bool queue_job(std::function<void()> job);
 		void fiber_tick();
+
+		[[nodiscard]] std::size_t pending_jobs() const;
+		[[nodiscard]] bool stopping() const noexcept
+		{
+			return m_stopping.load(std::memory_order_acquire);
+		}
+
 		static void fiber_func();
+
 	private:
-		std::recursive_mutex m_mutex;
-		std::stack<std::function<void()>> m_jobs;
+		mutable std::mutex m_mutex;
+		std::queue<std::function<void()>> m_jobs;
+		std::atomic_bool m_stopping{};
 	};
 
-	inline fiber_pool *g_fiber_pool{};
+	inline fiber_pool* g_fiber_pool{};
 }
