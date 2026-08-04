71de31672606d17eb3d183c0f4cce01902b00ee5
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-04 11:40:55 -0400

    menu: connect all page modules and polish layout

--- BigBaseV2/src/menu.cpp
+++ BigBaseV2/src/menu.cpp
@@ -1,8 +1,13 @@
 #include "menu.hpp"
 
 #include "menu/pages/lua.hpp"
+#include "menu/pages/players.hpp"
 #include "menu/pages/self.hpp"
 #include "menu/pages/settings.hpp"
+#include "menu/pages/teleport.hpp"
+#include "menu/pages/vehicle.hpp"
+#include "menu/pages/weapons.hpp"
+#include "menu/pages/world.hpp"
 
 #include <imgui.h>
 
@@ -10,8 +15,8 @@ namespace big
 {
 	void menu::draw()
 	{
-		ImGui::SetNextWindowSize(ImVec2(720.0f, 460.0f), ImGuiCond_FirstUseEver);
-		if (ImGui::Begin("BigBaseV2"))
+		ImGui::SetNextWindowSize(ImVec2(820.0f, 520.0f), ImGuiCond_FirstUseEver);
+		if (ImGui::Begin("BigBaseV2", nullptr, ImGuiWindowFlags_NoCollapse))
 		{
 			draw_sidebar();
 			ImGui::SameLine();
@@ -22,8 +27,13 @@ namespace big
 
 	void menu::draw_sidebar()
 	{
-		ImGui::BeginChild("Sidebar", ImVec2(150.0f, 0.0f), true);
+		ImGui::BeginChild("Sidebar", ImVec2(170.0f, 0.0f), true);
 
+		ImGui::Text("BigBaseV2");
+		ImGui::TextDisabled("Modular Menu Base");
+		ImGui::Separator();
+
+		ImGui::TextDisabled("Player");
 		if (ImGui::Selectable("Self", m_current_page == page::self))
 			m_current_page = page::self;
 		if (ImGui::Selectable("Weapons", m_current_page == page::weapons))
@@ -32,17 +42,28 @@ namespace big
 			m_current_page = page::vehicle;
 		if (ImGui::Selectable("Teleport", m_current_page == page::teleport))
 			m_current_page = page::teleport;
+
+		ImGui::Spacing();
+		ImGui::TextDisabled("Session");
 		if (ImGui::Selectable("World", m_current_page == page::world))
 			m_current_page = page::world;
 		if (ImGui::Selectable("Players", m_current_page == page::players))
 			m_current_page = page::players;
 
-		ImGui::Separator();
-		ImGui::TextDisabled("Settings");
+		ImGui::Spacing();
+		ImGui::TextDisabled("System");
 		if (ImGui::Selectable("Settings", m_current_page == page::settings))
 			m_current_page = page::settings;
 		if (ImGui::Selectable("Lua", m_current_page == page::lua))
 			m_current_page = page::lua;
+
+		const float footer_height = ImGui::GetTextLineHeightWithSpacing() * 2.0f;
+		if (ImGui::GetContentRegionAvail().y > footer_height)
+			ImGui::SetCursorPosY(ImGui::GetWindowHeight() - footer_height - ImGui::GetStyle().WindowPadding.y);
+
+		ImGui::Separator();
+		ImGui::TextDisabled("INSERT - Toggle Menu");
+		ImGui::TextDisabled("END - Unload");
 
 		ImGui::EndChild();
 	}
@@ -57,19 +78,19 @@ namespace big
 			menu_pages::draw_self();
 			break;
 		case page::weapons:
-			ImGui::Text("Weapons");
+			menu_pages::draw_weapons();
 			break;
 		case page::vehicle:
-			ImGui::Text("Vehicle");
+			menu_pages::draw_vehicle();
 			break;
 		case page::teleport:
-			ImGui::Text("Teleport");
+			menu_pages::draw_teleport();
 			break;
 		case page::world:
-			ImGui::Text("World");
+			menu_pages::draw_world();
 			break;
 		case page::players:
-			ImGui::Text("Players");
+			menu_pages::draw_players();
 			break;
 		case page::settings:
 			menu_pages::draw_settings();
