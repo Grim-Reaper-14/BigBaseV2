69b6ca5b675f5d12251baaffed76de5ad7aee4ae
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-04 18:06:57 -0400

    core: harden hook ownership and lifecycle

--- BigBaseV2/src/hooking.hpp
+++ BigBaseV2/src/hooking.hpp
@@ -7,56 +7,70 @@
 
 namespace big
 {
-	struct hooks
+	struct hooks final
 	{
 		static bool run_script_threads(std::uint32_t ops_to_execute);
-		static void *convert_thread_to_fiber(void *param);
+		static void* convert_thread_to_fiber(void* param);
 
-		static constexpr auto swapchain_num_funcs = 19;
-		static constexpr auto swapchain_present_index = 8;
-		static constexpr auto swapchain_resizebuffers_index = 13;
-		static HRESULT swapchain_present(IDXGISwapChain *this_, UINT sync_interval, UINT flags);
-		static HRESULT swapchain_resizebuffers(IDXGISwapChain *this_, UINT buffer_count, UINT width, UINT height, DXGI_FORMAT new_format, UINT swapchain_flags);
+		static constexpr std::size_t swapchain_num_funcs = 19;
+		static constexpr std::size_t swapchain_present_index = 8;
+		static constexpr std::size_t swapchain_resizebuffers_index = 13;
+		static HRESULT swapchain_present(IDXGISwapChain* swapchain, UINT sync_interval, UINT flags);
+		static HRESULT swapchain_resizebuffers(IDXGISwapChain* swapchain, UINT buffer_count, UINT width, UINT height, DXGI_FORMAT new_format, UINT swapchain_flags);
 
-		static LRESULT wndproc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+		static LRESULT wndproc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam);
 		static BOOL set_cursor_pos(int x, int y);
 
-		static constexpr auto main_persistent_num_funcs = 16;
-		static constexpr auto main_persistent_dtor_index = 0;
-		static constexpr auto main_persistent_is_networked_index = 6;
-		static void main_persistent_dtor(CGameScriptHandler *this_, bool free_memory);
-		static bool main_persistent_is_networked(CGameScriptHandler *this_);
+		static constexpr std::size_t main_persistent_num_funcs = 16;
+		static constexpr std::size_t main_persistent_dtor_index = 0;
+		static constexpr std::size_t main_persistent_is_networked_index = 6;
+		static void main_persistent_dtor(CGameScriptHandler* handler, bool free_memory);
+		static bool main_persistent_is_networked(CGameScriptHandler* handler);
 	};
 
-	struct minhook_keepalive
+	class minhook_keepalive final
 	{
+	public:
 		minhook_keepalive();
 		~minhook_keepalive();
+
+		minhook_keepalive(const minhook_keepalive&) = delete;
+		minhook_keepalive& operator=(const minhook_keepalive&) = delete;
 	};
 
-	class hooking
+	class hooking final
 	{
 		friend hooks;
+
 	public:
-		explicit hooking();
+		hooking();
 		~hooking();
 
-		void enable();
-		void disable();
+		hooking(const hooking&) = delete;
+		hooking(hooking&&) = delete;
+		hooking& operator=(const hooking&) = delete;
+		hooking& operator=(hooking&&) = delete;
 
+		void enable();
+		void disable() noexcept;
 		void ensure_dynamic_hooks();
+
+		[[nodiscard]] bool enabled() const noexcept
+		{
+			return m_enabled;
+		}
+
 	private:
 		bool m_enabled{};
 		minhook_keepalive m_minhook_keepalive;
 
 		vmt_hook m_swapchain_hook;
-		WNDPROC m_og_wndproc;
+		WNDPROC m_original_wndproc{};
 		detour_hook m_set_cursor_pos_hook;
-
 		detour_hook m_run_script_threads_hook;
 		detour_hook m_convert_thread_to_fiber_hook;
 		std::unique_ptr<vmt_hook> m_main_persistent_hook;
 	};
 
-	inline hooking *g_hooking{};
+	inline hooking* g_hooking{};
 }
