90a9772d25947465430e0489d354af71d7884c94
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-04 11:21:10 -0400

    menu: wire settings and Lua pages

--- BigBaseV2/src/menu.cpp
+++ BigBaseV2/src/menu.cpp
@@ -1,11 +1,16 @@
 #include "menu.hpp"
 
+#include "menu/pages/lua.hpp"
+#include "menu/pages/self.hpp"
+#include "menu/pages/settings.hpp"
+
 #include <imgui.h>
 
 namespace big
 {
 	void menu::draw()
 	{
+		ImGui::SetNextWindowSize(ImVec2(720.0f, 460.0f), ImGuiCond_FirstUseEver);
 		if (ImGui::Begin("BigBaseV2"))
 		{
 			draw_sidebar();
@@ -17,7 +22,7 @@ namespace big
 
 	void menu::draw_sidebar()
 	{
-		ImGui::BeginChild("Sidebar", ImVec2(150, 0), true);
+		ImGui::BeginChild("Sidebar", ImVec2(150.0f, 0.0f), true);
 
 		if (ImGui::Selectable("Self", m_current_page == page::self))
 			m_current_page = page::self;
@@ -31,20 +36,25 @@ namespace big
 			m_current_page = page::world;
 		if (ImGui::Selectable("Players", m_current_page == page::players))
 			m_current_page = page::players;
+
+		ImGui::Separator();
+		ImGui::TextDisabled("Settings");
 		if (ImGui::Selectable("Settings", m_current_page == page::settings))
 			m_current_page = page::settings;
+		if (ImGui::Selectable("Lua", m_current_page == page::lua))
+			m_current_page = page::lua;
 
 		ImGui::EndChild();
 	}
 
 	void menu::draw_page()
 	{
-		ImGui::BeginChild("Content", ImVec2(0, 0), true);
+		ImGui::BeginChild("Content", ImVec2(0.0f, 0.0f), true);
 
 		switch (m_current_page)
 		{
 		case page::self:
-			ImGui::Text("Self");
+			menu_pages::draw_self();
 			break;
 		case page::weapons:
 			ImGui::Text("Weapons");
@@ -62,7 +72,10 @@ namespace big
 			ImGui::Text("Players");
 			break;
 		case page::settings:
-			ImGui::Text("Settings");
+			menu_pages::draw_settings();
+			break;
+		case page::lua:
+			menu_pages::draw_lua();
 			break;
 		}
 
