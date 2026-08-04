a4548760391c8e869a53bca4edf5305711bfc9a9
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-04 11:09:53 -0400

    self: add state-backed player options

--- BigBaseV2/src/menu/pages/self.hpp
+++ BigBaseV2/src/menu/pages/self.hpp
@@ -1,6 +1,26 @@
 #pragma once
 
+#include <atomic>
+
 namespace big::menu_pages
 {
+	struct self_settings final
+	{
+		std::atomic_bool god_mode{false};
+		std::atomic_bool never_wanted{false};
+		std::atomic_bool no_ragdoll{false};
+		std::atomic_bool no_critical_hits{false};
+		std::atomic_bool unlimited_stamina{false};
+		std::atomic_bool super_jump{false};
+		std::atomic_bool invisible{false};
+		std::atomic_bool keep_clean{false};
+		std::atomic_bool fast_run{false};
+		std::atomic<float> run_multiplier{1.25f};
+	};
+
+	inline self_settings g_self_settings;
+
 	void draw_self();
+	void tick_self();
+	void self_script_func();
 }
