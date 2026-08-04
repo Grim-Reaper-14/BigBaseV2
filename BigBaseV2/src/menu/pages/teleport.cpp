94909f3bbafcfc982574be26553c67d6d9a16412
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-04 12:17:44 -0400

    menu: redesign teleport page

--- BigBaseV2/src/menu/pages/teleport.cpp
+++ BigBaseV2/src/menu/pages/teleport.cpp
@@ -1,4 +1,5 @@
 #include "teleport.hpp"
+#include "../widgets.hpp"
 
 #include <imgui.h>
 
@@ -6,30 +7,40 @@ namespace big::menu_pages
 {
 	void draw_teleport()
 	{
-		ImGui::Text("Teleport");
-		ImGui::Separator();
+		menu_ui::page_title("Teleport", "Move to common destinations, map markers, or precise world coordinates.");
 
-		ImGui::Checkbox("Keep Current Vehicle", &g_teleport_settings.keep_vehicle);
-		ImGui::Checkbox("Load Ground Before Teleport", &g_teleport_settings.load_ground);
-
-		ImGui::Spacing();
-		ImGui::TextDisabled("Quick Locations");
-		if (ImGui::Button("Waypoint", ImVec2(150.0f, 0.0f))) {}
-		ImGui::SameLine();
-		if (ImGui::Button("Objective", ImVec2(150.0f, 0.0f))) {}
-		if (ImGui::Button("Los Santos Customs", ImVec2(150.0f, 0.0f))) {}
-		ImGui::SameLine();
-		if (ImGui::Button("Airport", ImVec2(150.0f, 0.0f))) {}
-		if (ImGui::Button("Mount Chiliad", ImVec2(150.0f, 0.0f))) {}
-		ImGui::SameLine();
-		if (ImGui::Button("Fort Zancudo", ImVec2(150.0f, 0.0f))) {}
-
-		ImGui::Spacing();
-		ImGui::TextDisabled("Custom Coordinates");
-		ImGui::InputFloat3("Position", g_teleport_settings.custom_coordinates.data(), "%.3f");
-		if (ImGui::Button("Teleport To Coordinates", ImVec2(220.0f, 0.0f)))
+		if (menu_ui::begin_section("TeleportOptions", "Teleport Options", 105.0f))
 		{
-			// Action wiring is handled by the teleport feature layer.
+			ImGui::Checkbox("Keep Current Vehicle", &g_teleport_settings.keep_vehicle);
+			ImGui::Checkbox("Load Ground Before Teleport", &g_teleport_settings.load_ground);
 		}
+		menu_ui::end_section();
+
+		ImGui::Spacing();
+		if (menu_ui::begin_section("TeleportQuick", "Quick Locations", 165.0f))
+		{
+			if (ImGui::Button("Waypoint", ImVec2(155.0f, 0.0f))) {}
+			ImGui::SameLine();
+			if (ImGui::Button("Objective", ImVec2(155.0f, 0.0f))) {}
+			if (ImGui::Button("Los Santos Customs", ImVec2(155.0f, 0.0f))) {}
+			ImGui::SameLine();
+			if (ImGui::Button("Airport", ImVec2(155.0f, 0.0f))) {}
+			if (ImGui::Button("Mount Chiliad", ImVec2(155.0f, 0.0f))) {}
+			ImGui::SameLine();
+			if (ImGui::Button("Fort Zancudo", ImVec2(155.0f, 0.0f))) {}
+		}
+		menu_ui::end_section();
+
+		ImGui::Spacing();
+		if (menu_ui::begin_section("TeleportCustom", "Custom Coordinates", 135.0f))
+		{
+			ImGui::SetNextItemWidth(-1.0f);
+			ImGui::InputFloat3("##TeleportPosition", g_teleport_settings.custom_coordinates.data(), "%.3f");
+			if (ImGui::Button("Teleport To Coordinates", ImVec2(220.0f, 0.0f)))
+			{
+				// Action wiring is handled by the teleport feature layer.
+			}
+		}
+		menu_ui::end_section();
 	}
 }
