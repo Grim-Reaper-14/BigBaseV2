bff69f9246c1cc85e1c2653fa1e1c3532cefd6d4
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-04 18:06:24 -0400

    core: harden Enhanced native invoker state

--- BigBaseV2/src/invoker.hpp
+++ BigBaseV2/src/invoker.hpp
@@ -4,13 +4,14 @@
 
 namespace big
 {
-	class native_call_context : public rage::scrNativeCallContext
+	class native_call_context final : public rage::scrNativeCallContext
 	{
 	public:
 		native_call_context();
+
 	private:
-		std::uint64_t m_return_stack[10];
-		std::uint64_t m_arg_stack[100];
+		std::uint64_t m_return_stack[10]{};
+		std::uint64_t m_arg_stack[100]{};
 	};
 
 	struct native_cache_stats final
@@ -23,20 +24,31 @@ namespace big
 
 		[[nodiscard]] bool healthy() const noexcept
 		{
-			return mappings > 0 && cached > 0 && missing == 0 && duplicate_original_hashes == 0;
+			return mappings > 0 && cached == mappings && missing == 0 && duplicate_original_hashes == 0;
 		}
 	};
 
-	class native_invoker
+	class native_invoker final
 	{
 	public:
-		explicit native_invoker() = default;
+		native_invoker() = default;
 		~native_invoker() = default;
 
+		native_invoker(const native_invoker&) = delete;
+		native_invoker(native_invoker&&) = delete;
+		native_invoker& operator=(const native_invoker&) = delete;
+		native_invoker& operator=(native_invoker&&) = delete;
+
 		native_cache_stats cache_handlers();
+		void invalidate_cache() noexcept;
 
 		void begin_call();
 		void end_call(rage::scrNativeHash hash);
+
+		[[nodiscard]] bool ready() const noexcept
+		{
+			return m_cache_initialized && m_cache_stats.healthy() && !m_handler_cache.empty();
+		}
 
 		[[nodiscard]] const native_cache_stats& cache_stats() const noexcept
 		{
@@ -44,20 +56,22 @@ namespace big
 		}
 
 		template <typename T>
-		void push_arg(T &&value)
+		void push_arg(T&& value)
 		{
 			m_call_context.push_arg(std::forward<T>(value));
 		}
 
 		template <typename T>
-		T &get_return_value()
+		T& get_return_value()
 		{
 			return *m_call_context.get_return_value<T>();
 		}
+
 	private:
 		native_call_context m_call_context;
 		std::unordered_map<rage::scrNativeHash, rage::scrNativeHandler> m_handler_cache;
 		native_cache_stats m_cache_stats{};
+		bool m_cache_initialized{};
 	};
 
 	inline native_invoker g_native_invoker;
