0b760d0dd141552863474e261ae733e6212ce924
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-04 10:28:33 -0400

    Harden renderer initialization and shutdown

--- BigBaseV2/src/renderer.cpp
+++ BigBaseV2/src/renderer.cpp
@@ -4,6 +4,7 @@
 #include "gui.hpp"
 #include "pointers.hpp"
 #include "renderer.hpp"
+
 #include <imgui.h>
 #include <imgui_impl_dx11.h>
 #include <imgui_impl_win32.h>
@@ -12,31 +13,72 @@ IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARA
 
 namespace big
 {
-	renderer::renderer() :
-		m_dxgi_swapchain(*g_pointers->m_swapchain)
+	renderer::renderer()
 	{
-		void *d3d_device{};
-		if (SUCCEEDED(m_dxgi_swapchain->GetDevice(__uuidof(ID3D11Device), &d3d_device)))
+		if (g_pointers == nullptr || g_pointers->m_swapchain == nullptr || *g_pointers->m_swapchain == nullptr)
 		{
-			m_d3d_device.Attach(static_cast<ID3D11Device*>(d3d_device));
-		}
-		else
-		{
-			throw std::runtime_error("Failed to get D3D device.");
+			throw std::runtime_error("Renderer initialization failed: swap chain is unavailable.");
 		}
 
+		if (g_pointers->m_hwnd == nullptr)
+		{
+			throw std::runtime_error("Renderer initialization failed: game window is unavailable.");
+		}
+
+		m_dxgi_swapchain = *g_pointers->m_swapchain;
+
+		void* d3d_device{};
+		const HRESULT device_result = m_dxgi_swapchain->GetDevice(__uuidof(ID3D11Device), &d3d_device);
+		if (FAILED(device_result) || d3d_device == nullptr)
+		{
+			throw std::runtime_error("Renderer initialization failed: unable to acquire the D3D11 device.");
+		}
+
+		m_d3d_device.Attach(static_cast<ID3D11Device*>(d3d_device));
 		m_d3d_device->GetImmediateContext(m_d3d_device_context.GetAddressOf());
+		if (m_d3d_device_context == nullptr)
+		{
+			throw std::runtime_error("Renderer initialization failed: unable to acquire the D3D11 device context.");
+		}
 
+		IMGUI_CHECKVERSION();
 		ImGui::CreateContext();
-		ImGui_ImplDX11_Init(m_d3d_device.Get(), m_d3d_device_context.Get());
-		ImGui_ImplWin32_Init(g_pointers->m_hwnd);
+		ImGui::StyleColorsDark();
+
+		ImGuiIO& io = ImGui::GetIO();
+		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+
+		if (!ImGui_ImplWin32_Init(g_pointers->m_hwnd))
+		{
+			ImGui::DestroyContext();
+			throw std::runtime_error("Renderer initialization failed: ImGui Win32 backend could not start.");
+		}
+
+		if (!ImGui_ImplDX11_Init(m_d3d_device.Get(), m_d3d_device_context.Get()))
+		{
+			ImGui_ImplWin32_Shutdown();
+			ImGui::DestroyContext();
+			throw std::runtime_error("Renderer initialization failed: ImGui DX11 backend could not start.");
+		}
 
 		ImFontConfig font_cfg{};
 		font_cfg.FontDataOwnedByAtlas = false;
-		std::strcpy(font_cfg.Name, "Rubik");
+		strcpy_s(font_cfg.Name, "Rubik");
 
-		m_font = ImGui::GetIO().Fonts->AddFontFromMemoryTTF(const_cast<std::uint8_t*>(font_rubik), sizeof(font_rubik), 20.f, &font_cfg);
-		m_monospace_font = ImGui::GetIO().Fonts->AddFontDefault();
+		m_font = io.Fonts->AddFontFromMemoryTTF(
+			const_cast<std::uint8_t*>(font_rubik),
+			sizeof(font_rubik),
+			20.0f,
+			&font_cfg);
+		m_monospace_font = io.Fonts->AddFontDefault();
+
+		if (m_font == nullptr || m_monospace_font == nullptr)
+		{
+			ImGui_ImplDX11_Shutdown();
+			ImGui_ImplWin32_Shutdown();
+			ImGui::DestroyContext();
+			throw std::runtime_error("Renderer initialization failed: required fonts could not be loaded.");
+		}
 
 		g_gui.dx_init();
 		g_renderer = this;
@@ -44,34 +86,39 @@ namespace big
 
 	renderer::~renderer()
 	{
-		ImGui_ImplWin32_Shutdown();
-		ImGui_ImplDX11_Shutdown();
-		ImGui::DestroyContext();
-
 		g_renderer = nullptr;
+
+		if (ImGui::GetCurrentContext() != nullptr)
+		{
+			ImGui_ImplDX11_Shutdown();
+			ImGui_ImplWin32_Shutdown();
+			ImGui::DestroyContext();
+		}
+
+		m_d3d_device_context.Reset();
+		m_d3d_device.Reset();
+		m_dxgi_swapchain.Reset();
 	}
 
 	void renderer::on_present()
 	{
+		if (ImGui::GetCurrentContext() == nullptr)
+			return;
+
+		ImGuiIO& io = ImGui::GetIO();
+		io.MouseDrawCursor = g_gui.m_opened;
+
 		if (g_gui.m_opened)
-		{
-			ImGui::GetIO().MouseDrawCursor = true;
-			ImGui::GetIO().ConfigFlags &= ~ImGuiConfigFlags_NoMouse;
-		}
+			io.ConfigFlags &= ~ImGuiConfigFlags_NoMouse;
 		else
-		{
-			ImGui::GetIO().MouseDrawCursor = false;
-			ImGui::GetIO().ConfigFlags |= ImGuiConfigFlags_NoMouse;
-		}
+			io.ConfigFlags |= ImGuiConfigFlags_NoMouse;
 
 		ImGui_ImplDX11_NewFrame();
 		ImGui_ImplWin32_NewFrame();
 		ImGui::NewFrame();
 
 		if (g_gui.m_opened)
-		{
 			g_gui.dx_on_tick();
-		}
 
 		ImGui::Render();
 		ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
@@ -79,22 +126,22 @@ namespace big
 
 	void renderer::pre_reset()
 	{
-		ImGui_ImplDX11_InvalidateDeviceObjects();
+		if (ImGui::GetCurrentContext() != nullptr)
+			ImGui_ImplDX11_InvalidateDeviceObjects();
 	}
 
 	void renderer::post_reset()
 	{
-		ImGui_ImplDX11_CreateDeviceObjects();
+		if (ImGui::GetCurrentContext() != nullptr)
+			ImGui_ImplDX11_CreateDeviceObjects();
 	}
 
 	void renderer::wndproc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 	{
 		if (msg == WM_KEYUP && wparam == VK_INSERT)
-			g_gui.m_opened ^= true;
+			g_gui.m_opened = !g_gui.m_opened;
 
-		if (g_gui.m_opened)
-		{
+		if (g_gui.m_opened && ImGui::GetCurrentContext() != nullptr)
 			ImGui_ImplWin32_WndProcHandler(hwnd, msg, wparam, lparam);
-		}
 	}
 }
