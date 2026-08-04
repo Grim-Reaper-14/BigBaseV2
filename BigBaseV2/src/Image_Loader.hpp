f6896a478eae80660d847cca65c55031686f351c
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-04 18:57:24 -0400

    graphics: add D3D11 image loader interface

---
+++ BigBaseV2/src/Image_Loader.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "common.hpp"
+
+namespace big
+{
+	class Image_Loader final
+	{
+	public:
+		Image_Loader() = default;
+		~Image_Loader() = default;
+
+		Image_Loader(const Image_Loader&) = delete;
+		Image_Loader(Image_Loader&&) noexcept = default;
+		Image_Loader& operator=(const Image_Loader&) = delete;
+		Image_Loader& operator=(Image_Loader&&) noexcept = default;
+
+		bool LoadFromFile(ID3D11Device* device, const std::filesystem::path& path, std::string& error);
+		bool LoadFromMemory(ID3D11Device* device, const void* data, std::size_t size, std::string& error);
+		void Reset() noexcept;
+
+		[[nodiscard]] bool IsLoaded() const noexcept;
+		[[nodiscard]] ID3D11ShaderResourceView* GetShaderResourceView() const noexcept;
+		[[nodiscard]] ImTextureID GetImGuiTextureID() const noexcept;
+		[[nodiscard]] std::uint32_t GetWidth() const noexcept;
+		[[nodiscard]] std::uint32_t GetHeight() const noexcept;
+		[[nodiscard]] const std::filesystem::path& GetSourcePath() const noexcept;
+
+	private:
+		bool CreateTexture(
+			ID3D11Device* device,
+			const std::uint8_t* pixels,
+			std::uint32_t width,
+			std::uint32_t height,
+			std::string& error);
+
+		comptr<ID3D11Texture2D> m_texture;
+		comptr<ID3D11ShaderResourceView> m_shader_resource_view;
+		std::filesystem::path m_source_path;
+		std::uint32_t m_width{};
+		std::uint32_t m_height{};
+	};
+}
