04698423d908cdeee3ba462e68423f1ffede022f
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-04 19:33:39 -0400

    memory: harden module interface

--- BigBaseV2/src/memory/module.hpp
+++ BigBaseV2/src/memory/module.hpp
@@ -1,16 +1,25 @@
 #pragma once
+
+#include <string_view>
+
 #include "range.hpp"
 
 namespace memory
 {
-	class module : public range
+	class module final : public range
 	{
 	public:
-		module(HMODULE mod);
-		explicit module(std::nullptr_t);
-		explicit module(std::string_view name);
-		explicit module(std::wstring_view name);
+		module() noexcept = default;
+		explicit module(HMODULE module_handle) noexcept;
+		explicit module(std::nullptr_t) noexcept;
+		explicit module(std::string_view name) noexcept;
+		explicit module(std::wstring_view name) noexcept;
 
-		memory::handle get_export(std::string_view symbol_name);
+		[[nodiscard]] handle get_export(std::string_view symbol_name) const noexcept;
+		[[nodiscard]] HMODULE native_handle() const noexcept;
+		[[nodiscard]] bool valid() const noexcept;
+
+	private:
+		void initialize(HMODULE module_handle) noexcept;
 	};
 }
