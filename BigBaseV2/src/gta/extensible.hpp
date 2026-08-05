fa9a5aee4419bb8d1748383be1b55c6f1564440e
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-05 03:36:42 -0400

    Fix GTA extensible type hashing

--- BigBaseV2/src/gta/extensible.hpp
+++ BigBaseV2/src/gta/extensible.hpp
@@ -1,6 +1,11 @@
 #pragma once
-#include <cstdint>
+
+#include "joaat.hpp"
 #include "ref_aware.hpp"
+
+#include <cstdint>
+#include <string_view>
+#include <typeinfo>
 
 namespace rage
 {
@@ -8,37 +13,43 @@ namespace rage
 	{
 	public:
 		virtual ~fwExtension() = default;
-		virtual void unk_0x08() = 0;
-		virtual void unk_0x10() = 0;
+		virtual void unknown_0x08() = 0;
+		virtual void unknown_0x10() = 0;
 		virtual std::uint32_t get_id() = 0;
 	};
 
-	class fwExtensionContainer
+	struct fwExtensionContainer
 	{
-	public:
-		fwExtension *m_entry;
-		fwExtensionContainer* m_next;
+		fwExtension* m_entry{};
+		fwExtensionContainer* m_next{};
 	};
 
 	class fwExtensibleBase : public fwRefAwareBase
 	{
 	public:
 		virtual bool is_of_type(std::uint32_t hash) = 0;
-		virtual std::uint32_t const &get_type() = 0;
-	public:
-		fwExtensionContainer* m_extension_container; // 0x10
-	private:
-		void *m_extensible_unk;                      // 0x18
-	public:
-		template <typename T>
-		bool is_of_type()
-		{
-			static auto name = (typeid(T).name()) + 6; // Skip "class "
-			static auto name_hash = util::joaat(name);
+		virtual const std::uint32_t& get_type() = 0;
 
-			return is_of_type(name_hash);
+		template <typename T>
+		[[nodiscard]] bool is_of_type()
+		{
+			static const auto type_hash = []
+			{
+				std::string_view name{typeid(T).name()};
+				if (name.compare(0, 6, "class ") == 0)
+					name.remove_prefix(6);
+				else if (name.compare(0, 7, "struct ") == 0)
+					name.remove_prefix(7);
+				return rage::joaat(name);
+			}();
+
+			return is_of_type(type_hash);
 		}
+
+		fwExtensionContainer* m_extension_container{}; // 0x10
+		void* m_extensible_unknown{};                   // 0x18
 	};
 
+	static_assert(sizeof(fwExtensionContainer) == 0x10);
 	static_assert(sizeof(fwExtensibleBase) == 0x20);
 }
