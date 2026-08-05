09914a096f770bfc2a7f9556173cc38ed1591fbd
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-05 03:36:11 -0400

    Harden JOAAT hashing for GTA Enhanced

--- BigBaseV2/src/gta/joaat.hpp
+++ BigBaseV2/src/gta/joaat.hpp
@@ -1,84 +1,62 @@
 #pragma once
+
 #include <cstddef>
 #include <cstdint>
 #include <string_view>
 #include <type_traits>
+#include <utility>
 
 namespace rage
 {
 	using joaat_t = std::uint32_t;
 
-	inline constexpr char joaat_to_lower(char c)
+	[[nodiscard]] inline constexpr char joaat_to_lower(char value) noexcept
 	{
-		return (c >= 'A' && c <= 'Z') ? c + ('a' - 'A') : c;
+		return value >= 'A' && value <= 'Z' ? static_cast<char>(value + ('a' - 'A')) : value;
 	}
 
-	template <std::size_t CharCount>
+	[[nodiscard]] inline constexpr joaat_t joaat_finalize(joaat_t hash) noexcept
+	{
+		hash += hash << 3;
+		hash ^= hash >> 11;
+		hash += hash << 15;
+		return hash;
+	}
+
+	[[nodiscard]] inline constexpr joaat_t joaat(std::string_view value) noexcept
+	{
+		joaat_t hash = 0;
+		for (const char character : value)
+		{
+			hash += static_cast<std::uint8_t>(joaat_to_lower(character));
+			hash += hash << 10;
+			hash ^= hash >> 6;
+		}
+		return joaat_finalize(hash);
+	}
+
+	[[nodiscard]] inline joaat_t joaat(const char* value) noexcept
+	{
+		return value ? joaat(std::string_view{value}) : 0;
+	}
+
+	template <std::size_t CharacterCount>
 	struct constexpr_joaat
 	{
-		char data[CharCount];
+		char data[CharacterCount]{};
 
 		template <std::size_t... Indices>
-		constexpr constexpr_joaat(const char *str, std::index_sequence<Indices...>) :
-			data{ (str[Indices])... }
+		constexpr constexpr_joaat(const char* value, std::index_sequence<Indices...>) noexcept :
+			data{value[Indices]...}
 		{
 		}
 
-		constexpr joaat_t operator()()
+		[[nodiscard]] constexpr joaat_t operator()() const noexcept
 		{
-			joaat_t hash = 0;
-
-			for (std::size_t i = 0; i < CharCount; ++i)
-			{
-				hash += joaat_to_lower(data[i]);
-				hash += (hash << 10);
-				hash ^= (hash >> 6);
-			}
-
-			hash += (hash << 3);
-			hash ^= (hash >> 11);
-			hash += (hash << 15);
-
-			return hash;
+			return joaat(std::string_view{data, CharacterCount});
 		}
 	};
-
-	inline joaat_t joaat(std::string_view str)
-	{
-		joaat_t hash = 0;
-
-		for (char c : str)
-		{
-			hash += joaat_to_lower(c);
-			hash += (hash << 10);
-			hash ^= (hash >> 6);
-		}
-
-		hash += (hash << 3);
-		hash ^= (hash >> 11);
-		hash += (hash << 15);
-
-		return hash;
-	}
-
-	inline joaat_t joaat(const char *str)
-	{
-		joaat_t hash = 0;
-
-		while (*str)
-		{
-			hash += joaat_to_lower(*(str++));
-			hash += (hash << 10);
-			hash ^= (hash >> 6);
-		}
-
-		hash += (hash << 3);
-		hash ^= (hash >> 11);
-		hash += (hash << 15);
-
-		return hash;
-	}
 }
 
-#define RAGE_JOAAT_IMPL(str) (::rage::constexpr_joaat<sizeof(str) - 1>((str), std::make_index_sequence<sizeof(str) - 1>())())
-#define RAGE_JOAAT(str) (std::integral_constant<rage::joaat_t, RAGE_JOAAT_IMPL(str)>::value)
+#define RAGE_JOAAT_IMPL(value) (::rage::constexpr_joaat<sizeof(value) - 1>((value), std::make_index_sequence<sizeof(value) - 1>{})())
+#define RAGE_JOAAT(value) (std::integral_constant<::rage::joaat_t, RAGE_JOAAT_IMPL(value)>::value)
