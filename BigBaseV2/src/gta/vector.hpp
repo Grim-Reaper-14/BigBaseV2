4a88924050a47f9677d2094a2737f80649295d9a
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-05 03:31:27 -0400

    Update GTA Enhanced vector layouts

--- BigBaseV2/src/gta/vector.hpp
+++ BigBaseV2/src/gta/vector.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <cmath>
+#include <cstddef>
+
 namespace rage
 {
 	struct vector2
@@ -8,11 +11,39 @@ namespace rage
 		float y{};
 	};
 
-	struct vector3
+	// GTA V Enhanced aligns three-component engine vectors to 16 bytes.
+	struct alignas(16) vector3
 	{
 		float x{};
 		float y{};
 		float z{};
+
+		[[nodiscard]] constexpr bool operator==(const vector3& other) const noexcept = default;
+
+		[[nodiscard]] constexpr vector3 operator+(const vector3& other) const noexcept
+		{
+			return {x + other.x, y + other.y, z + other.z};
+		}
+
+		[[nodiscard]] constexpr vector3 operator-(const vector3& other) const noexcept
+		{
+			return {x - other.x, y - other.y, z - other.z};
+		}
+
+		[[nodiscard]] constexpr vector3 operator*(float scalar) const noexcept
+		{
+			return {x * scalar, y * scalar, z * scalar};
+		}
+
+		[[nodiscard]] float magnitude() const noexcept
+		{
+			return std::sqrt((x * x) + (y * y) + (z * z));
+		}
+
+		[[nodiscard]] float distance(const vector3& other) const noexcept
+		{
+			return (*this - other).magnitude();
+		}
 	};
 
 	struct vector4
@@ -23,27 +54,49 @@ namespace rage
 		float w{};
 	};
 
-#pragma pack(push, 1)
-	class scrVector
+	using fvector2 = vector2;
+	using fvector3 = vector3;
+	using fvector4 = vector4;
+
+	// Script vectors use one eight-byte slot per component in native calls.
+	class scrVector final
 	{
 	public:
-		scrVector() = default;
+		constexpr scrVector() = default;
+		constexpr scrVector(float x_value, float y_value, float z_value) noexcept :
+			x(x_value),
+			y(y_value),
+			z(z_value)
+		{
+		}
 
-		scrVector(float x, float y, float z) :
-			x(x), y(y), z(z)
-		{}
-	public:
-		float x{};
-	private:
-		char m_padding1[0x04];
-	public:
-		float y{};
-	private:
-		char m_padding2[0x04];
-	public:
-		float z{};
-	private:
-		char m_padding3[0x04];
+		constexpr scrVector(const fvector3& value) noexcept :
+			x(value.x),
+			y(value.y),
+			z(value.z)
+		{
+		}
+
+		[[nodiscard]] constexpr operator fvector3() const noexcept
+		{
+			return {x, y, z};
+		}
+
+		[[nodiscard]] constexpr explicit operator bool() const noexcept
+		{
+			return x != 0.0f || y != 0.0f || z != 0.0f;
+		}
+
+		alignas(8) float x{};
+		alignas(8) float y{};
+		alignas(8) float z{};
 	};
-#pragma pack(pop)
+
+	static_assert(sizeof(vector2) == 0x08);
+	static_assert(sizeof(vector3) == 0x10);
+	static_assert(sizeof(vector4) == 0x10);
+	static_assert(offsetof(scrVector, x) == 0x00);
+	static_assert(offsetof(scrVector, y) == 0x08);
+	static_assert(offsetof(scrVector, z) == 0x10);
+	static_assert(sizeof(scrVector) == 0x18);
 }
