4c06feed7d3afc44621d174d0d5541d07872b5cb
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-04 22:28:57 -0400

    runtime: add pointer resolution diagnostics

--- BigBaseV2/src/pointers.hpp
+++ BigBaseV2/src/pointers.hpp
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "common.hpp"
 #include "function_types.hpp"
 #include "gta/enums.hpp"
@@ -9,6 +10,23 @@ namespace big
 	class pointers final
 	{
 	public:
+		struct resolution_report final
+		{
+			std::uintptr_t module_base{};
+			std::size_t module_size{};
+			std::size_t required_total{};
+			std::size_t required_found{};
+			std::size_t optional_total{};
+			std::size_t optional_found{};
+			std::chrono::milliseconds elapsed{};
+			std::vector<std::string> missing_optional;
+
+			[[nodiscard]] bool required_complete() const noexcept
+			{
+				return required_total > 0 && required_found == required_total;
+			}
+		};
+
 		pointers();
 		~pointers();
 
@@ -20,6 +38,12 @@ namespace big
 		[[nodiscard]] bool core_ready() const noexcept;
 		[[nodiscard]] bool renderer_ready() const noexcept;
 		[[nodiscard]] bool scripts_ready() const noexcept;
+		[[nodiscard]] bool native_ready() const noexcept;
+		[[nodiscard]] bool network_ready() const noexcept;
+		[[nodiscard]] bool legacy_native_lookup_ready() const noexcept;
+		[[nodiscard]] bool fully_ready() const noexcept;
+		[[nodiscard]] const resolution_report& report() const noexcept;
+		[[nodiscard]] std::vector<std::string> missing_required() const;
 		void validate_required() const;
 
 		HWND m_hwnd{};
@@ -30,7 +54,7 @@ namespace big
 		CPedFactory** m_ped_factory{};
 		CNetworkPlayerMgr** m_network_player_mgr{};
 
-		// Legacy registration lookup is retained temporarily for diagnostics.
+		// Optional legacy registration lookup retained for diagnostics only.
 		rage::scrNativeRegistrationTable* m_native_registration_table{};
 		functions::get_native_handler_t m_get_native_handler{};
 
@@ -45,6 +69,9 @@ namespace big
 
 		CGameScriptHandlerMgr** m_script_handler_mgr{};
 		IDXGISwapChain** m_swapchain{};
+
+	private:
+		resolution_report m_report;
 	};
 
 	inline pointers* g_pointers{};
