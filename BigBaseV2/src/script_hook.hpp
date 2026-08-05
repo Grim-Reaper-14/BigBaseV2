268a4bbfa1d0ef04f132070cd16f7fbbdf6c94f2
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-05 03:35:04 -0400

    Prepare script hooks for GTA Enhanced programs

--- BigBaseV2/src/script_hook.hpp
+++ BigBaseV2/src/script_hook.hpp
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "common.hpp"
 #include "gta/fwddec.hpp"
 #include "gta/joaat.hpp"
@@ -7,23 +8,41 @@
 
 namespace big
 {
-	class script_hook
+	class script_hook final
 	{
 	public:
-		explicit script_hook(rage::joaat_t script_hash, std::unordered_map<rage::scrNativeHash, rage::scrNativeHandler> native_replacements);
+		explicit script_hook(
+			rage::joaat_t script_hash,
+			std::unordered_map<rage::scrNativeHash, rage::scrNativeHandler> native_replacements);
 		~script_hook();
 
-		void ensure();
-	private:
-		void hook_instance(rage::scrProgram *program);
+		script_hook(const script_hook&) = delete;
+		script_hook(script_hook&&) = delete;
+		script_hook& operator=(const script_hook&) = delete;
+		script_hook& operator=(script_hook&&) = delete;
 
-		static void scrprogram_dtor(rage::scrProgram *this_, bool free_memory);
+		void ensure();
+
+	private:
+		struct native_patch final
+		{
+			rage::scrNativeHash hash{};
+			rage::scrNativeHandler* slot{};
+			rage::scrNativeHandler original{};
+			rage::scrNativeHandler replacement{};
+		};
+
+		void hook_instance(rage::scrProgram* program);
+		void restore_handlers() noexcept;
+		void detach() noexcept;
+
+		static void scrprogram_dtor(rage::scrProgram* this_, bool free_memory);
 		static std::unordered_map<rage::scrProgram*, script_hook*> s_map;
 
-		rage::joaat_t m_script_hash;
-		rage::scrProgram* m_program;
+		rage::joaat_t m_script_hash{};
+		rage::scrProgram* m_program{};
 		std::unique_ptr<vmt_hook> m_vmt_hook;
 		std::unordered_map<rage::scrNativeHash, rage::scrNativeHandler> m_native_replacements;
-		std::unordered_map<rage::scrNativeHash, rage::scrNativeHandler*> m_native_handler_ptrs;
+		std::vector<native_patch> m_native_patches;
 	};
 }
