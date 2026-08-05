f192391604dff651077aae2a81c7f7afdddd0bc5
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-05 03:33:59 -0400

    Update the GTA Enhanced ped factory vtable

--- BigBaseV2/src/gta/ped_factory.hpp
+++ BigBaseV2/src/gta/ped_factory.hpp
@@ -1,18 +1,62 @@
 #pragma once
+
 #include "fwddec.hpp"
+
+#include <cstdint>
 
 #pragma pack(push, 1)
 class CPed
 {
 public:
 	char m_padding[0x10B8];
-	CPlayerInfo *m_playerinfo;
+	CPlayerInfo* m_playerinfo;
 };
 #pragma pack(pop)
 
 class CPedFactory
 {
 public:
+	enum class ped_create_flags : std::uint8_t
+	{
+		is_networked = 1u << 0,
+		is_player = 1u << 1,
+	};
+
 	virtual ~CPedFactory() = default;
-	CPed *m_local_ped;
+	virtual CPed* create_ped(
+		std::uint8_t* flags,
+		std::uint16_t* model_index,
+		void* matrix,
+		bool default_component_variation,
+		bool register_network_object,
+		bool give_default_loadout,
+		bool,
+		bool) = 0;
+	virtual CPed* create_clone(
+		std::uint8_t* flags,
+		std::uint16_t* model_index,
+		void* matrix,
+		bool default_component_variation,
+		bool,
+		bool register_network_object,
+		bool) = 0;
+	virtual CPed* clone_ped(
+		CPed* ped,
+		bool register_network_object,
+		bool link_blends,
+		bool clone_compressed_damage) = 0;
+	virtual CPed* clone_ped_to_target(
+		CPed* source,
+		CPed* target,
+		bool clone_compressed_damage) = 0;
+	virtual CPed* create_player(
+		std::uint8_t* flags,
+		std::uint16_t model_index,
+		void* matrix,
+		CPlayerInfo* player_info) = 0;
+	virtual void destroy_ped(CPed* ped) = 0;
+
+	CPed* m_local_ped{}; // 0x08
 };
+
+static_assert(sizeof(CPedFactory) == 0x10);
