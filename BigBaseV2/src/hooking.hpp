06e50fb30d86578659476b1e2000cbb6be414777
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-05 05:47:51 -0400

    Separate main loop from auxiliary hooks

--- BigBaseV2/src/hooking.hpp
+++ BigBaseV2/src/hooking.hpp
@@ -1,15 +1,12 @@
 #pragma once
 #include "common.hpp"
 #include "detour_hook.hpp"
-#include "gta/fwddec.hpp"
-#include "script_hook.hpp"
 #include "vmt_hook.hpp"
 
 namespace big
 {
 	struct hooks final
 	{
-		static bool run_script_threads(std::uint32_t ops_to_execute);
 		static void* convert_thread_to_fiber(void* param);
 
 		static constexpr std::size_t swapchain_num_funcs = 19;
@@ -20,12 +17,6 @@ namespace big
 
 		static LRESULT wndproc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam);
 		static BOOL set_cursor_pos(int x, int y);
-
-		static constexpr std::size_t main_persistent_num_funcs = 16;
-		static constexpr std::size_t main_persistent_dtor_index = 0;
-		static constexpr std::size_t main_persistent_is_networked_index = 6;
-		static void main_persistent_dtor(CGameScriptHandler* handler, bool free_memory);
-		static bool main_persistent_is_networked(CGameScriptHandler* handler);
 	};
 
 	class minhook_keepalive final
@@ -38,6 +29,8 @@ namespace big
 		minhook_keepalive& operator=(const minhook_keepalive&) = delete;
 	};
 
+	// Owns renderer, input, and fiber compatibility hooks. The game's main
+	// script-loop detour is owned exclusively by main_hook.
 	class hooking final
 	{
 		friend hooks;
@@ -53,7 +46,6 @@ namespace big
 
 		void enable();
 		void disable() noexcept;
-		void ensure_dynamic_hooks();
 
 		[[nodiscard]] bool enabled() const noexcept
 		{
@@ -67,9 +59,7 @@ namespace big
 		vmt_hook m_swapchain_hook;
 		WNDPROC m_original_wndproc{};
 		detour_hook m_set_cursor_pos_hook;
-		detour_hook m_run_script_threads_hook;
 		detour_hook m_convert_thread_to_fiber_hook;
-		std::unique_ptr<vmt_hook> m_main_persistent_hook;
 	};
 
 	inline hooking* g_hooking{};
