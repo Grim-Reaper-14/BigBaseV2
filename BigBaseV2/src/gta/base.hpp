a878f569b0248eb3c5852597c8616e8a377e6149
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-05 03:32:57 -0400

    Update the GTA Enhanced base class vtable

--- BigBaseV2/src/gta/base.hpp
+++ BigBaseV2/src/gta/base.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdint>
+
 namespace rage
 {
 	class datBase
@@ -8,11 +10,25 @@ namespace rage
 		virtual ~datBase() = default;
 	};
 
+	// GTA V Enhanced pgBase exposes five paging/metadata virtual functions.
+	// It does not place a virtual destructor in slot zero.
 	class pgBase
 	{
 	public:
-		virtual ~pgBase() = default;
-	private:
-		void *m_pgunk;
+		virtual void shutdown_class() {}
+		virtual void set_handle_index(std::uint32_t) {}
+		virtual void validate(std::uint32_t) {}
+		virtual void init_class(std::int32_t) {}
+		virtual std::uint32_t get_handle_index() const
+		{
+			return 0;
+		}
+
+	protected:
+		std::uint32_t m_map_size{};
+		std::uint32_t m_padding{};
 	};
+
+	static_assert(sizeof(datBase) == 0x08);
+	static_assert(sizeof(pgBase) == 0x10);
 }
